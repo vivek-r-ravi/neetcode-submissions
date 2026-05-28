ac1dc266ea9e3821c29afae7abaed7b39757551e
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-05-27 23:51:31 -0500

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.py
@@ -0,0 +1,27 @@
+from collections import deque, Counter
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        
+        #solution 1: brute force simulation with a queue O(n2)
+        '''
+        students = deque(students)
+        sandwiches = deque(sandwiches)
+        while len(students)>0 and sandwiches[0] in students:
+            if students[0]==sandwiches[0]:
+                students.popleft()
+                sandwiches.popleft()
+            else:
+                students.append(students[0])
+                students.popleft()
+        return len(students)
+        '''
+        
+        #solution 2: efficient solution using counting O(n)
+        student_count = Counter(students)
+        for s in sandwiches:
+            if student_count[s]>0:
+                student_count[s]-=1
+            else:
+                return sum(student_count.values())
+        return 0
