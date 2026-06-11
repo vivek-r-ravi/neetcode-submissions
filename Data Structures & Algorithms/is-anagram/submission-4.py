3d955ab03783797324c1d2f79ca757283fcf15fd
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-11 15:18:07 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,10 @@
+# naive solution: sorting both strings and compare. O(mlogm + nlogn) time.
+
+# canonical solution: compare the two counter dictionaries
+# O(m+n) time and O(1) space as there are only 26 alphabets
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        return Counter(s)==Counter(t)
