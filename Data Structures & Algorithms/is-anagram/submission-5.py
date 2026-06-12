c611e1991321f704d298d3601238f3711ee7d94d
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-11 23:13:38 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,11 @@
+# naive solution: sorting both strings and compare. O(mlogm + nlogn) time.
+# alternate solution: bucket sort both strings and compare. O(m+n) time.
+
+# canonical solution: compare the two counter dictionaries
+# O(m+n) time and O(1) space as there are only 26 alphabets
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        return Counter(s)==Counter(t)
