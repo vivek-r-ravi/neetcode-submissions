15624b08e95e7cf5b7fff3fa068daba1de80ba93
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-12 14:46:28 -0500

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows,cols=len(grid),len(grid[0])
+        neighbors=[[-1,0],[1,0],[0,-1],[0,1]]
+        q=deque([(r,c) for r in range(rows) for c in range(cols) if grid[r][c]==2])
+        fresh_count=sum([1 for r in range(rows) for c in range(cols) if grid[r][c]==1])
+        if not q and not fresh_count:
+            return 0
+        if not q:
+            return -1
+        
+        minutes=-1
+        while q:
+            for i in range(len(q)):
+                r,c=q.popleft()
+                for dr,dc in neighbors:
+                    nr,nc=r+dr,c+dc
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
+                        grid[nr][nc]=2
+                        fresh_count-=1
+                        q.append((nr,nc))
+            minutes+=1
+        
+        if fresh_count:
+            return -1
+        else:
+            return minutes
