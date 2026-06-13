b6e38f895e11851a62d27351f6508b8401df05bc
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-12 21:31:45 -0500

    Add: graph - submission-2

---
+++ Data Structures & Algorithms/graph/submission-2.py
@@ -0,0 +1,40 @@
+# Adjacency List is represented as a hashmap
+# keys are vertices, values are a set (instead of list due to O(1) remove)
+# O(1) time and O(V+E) space for addEdge and removeEdge
+# O(V+E) time and space for hasPath BFS. BFS preferred as it finds shortest path.
+class Graph:
+    
+    def __init__(self):
+        self.adjList={}
+
+    def addEdge(self, src: int, dst: int) -> None:
+        if src not in self.adjList:
+            self.adjList[src]=set()
+        if dst not in self.adjList:
+            self.adjList[dst]=set()
+        self.adjList[src].add(dst)
+
+    def removeEdge(self, src: int, dst: int) -> bool:
+        if src not in self.adjList:
+            self.adjList[src]=set()
+        if dst not in self.adjList:
+            self.adjList[dst]=set()
+        if dst in self.adjList[src]:
+            self.adjList[src].remove(dst)
+            return True
+        return False
+
+    # BFS Solution O(V+E) time and space
+    # visited set not used here, as 
+    def hasPath(self, src: int, dst: int) -> bool:
+        q=deque([src])
+        visit=set([src])
+        while q:
+            vertex=q.popleft()
+            if vertex==dst:
+                return True
+            for neighbor in self.adjList[vertex]:
+                if neighbor not in visit:
+                    visit.add(neighbor)
+                    q.append(neighbor)
+        return False
