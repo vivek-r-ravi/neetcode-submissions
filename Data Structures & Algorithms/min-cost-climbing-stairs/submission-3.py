f5fcc2615954dc73094757eacbaae94d9df98da8
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-14 23:50:21 -0500

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,32 @@
+# solution 1: brute force recursion
+# O(2^n) time and O(n) space due to recursion stack
+class SolutionV1:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n=len(cost)
+        if n==1:
+            return 0
+        if n==2:
+            return min(cost[0],cost[1])
+        return min(
+            self.minCostClimbingStairs(cost[:-2])+cost[-2],
+            self.minCostClimbingStairs(cost[:-1])+cost[-1]
+            )
+
+# solution 2: top-down DP
+# O(n) time and space
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cache={1:0,2:min(cost[0],cost[1])}
+        def memoization(cost,cache,idx) -> int:
+            if idx in cache:
+                return cache[idx]
+            cache[idx]=min(
+                memoization(cost,cache,idx-2)+cost[idx-2],
+                memoization(cost,cache,idx-1)+cost[idx-1]
+            )
+            print(cache)
+            return cache[idx]
+        return memoization(cost,cache,len(cost))
+
+# solution 3: bottom-up DP
+# O(n) time and O(1) space
