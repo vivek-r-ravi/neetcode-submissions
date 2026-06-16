95c1cdcc5cb15203790b410a75cb207412308eb2
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-15 22:04:35 -0500

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.py
@@ -0,0 +1,24 @@
+class SolutionV1:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows,cols=len(grid),len(grid[0])
+        def dfs(r,c):
+            if r==0 and c==0:
+                return grid[0][0]
+            if r<0 or c<0:
+                return float('inf')
+            return min(dfs(r-1,c),dfs(r,c-1))+grid[r][c]
+        return dfs(rows-1,cols-1)
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows,cols=len(grid),len(grid[0])
+        cache=[[float('inf')]*cols for _ in range(rows)]
+        cache[0][0]=grid[0][0]
+        def dfs(r,c):
+            if r<0 or c<0:
+                return float('inf')
+            if cache[r][c]!=float('inf'):
+                return cache[r][c]
+            cache[r][c]=min(dfs(r-1,c),dfs(r,c-1))+grid[r][c]
+            return cache[r][c]
+        return dfs(rows-1,cols-1)
