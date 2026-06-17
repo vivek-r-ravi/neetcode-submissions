267bebd126fc5f5123b2610722c8144a4da497c3
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-17 16:33:40 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        curr = []
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        for p in s:
+            if p in pairs:
+                if curr and pairs[p]==curr[-1]:
+                    curr.pop()
+                else:
+                    return False
+            else:
+                curr.append(p)
+        return len(curr)==0
