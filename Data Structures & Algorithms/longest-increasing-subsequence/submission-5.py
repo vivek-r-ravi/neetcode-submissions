c64d6f594e0fb44b1c07e1572a0da70a0c5b041e
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-17 01:07:32 -0500

    Add: longest-increasing-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-5.py
@@ -0,0 +1,68 @@
+# brute force recursion
+# exponential time, O(n) space
+class SolutionV1:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        # state: dfs(i) is length of LIS at end of i index in nums
+        def dfs(i):
+            if i==0:
+                return 1
+            best=1
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    best=max(best,1+dfs(j))
+            return best
+        return max(dfs(i) for i in range(n))
+
+# memoization
+# O(n2) time, O(n) space
+class SolutionV2:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        cache={0:1}
+        def dfs(i):
+            if i in cache:
+                return cache[i]
+            best=1
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    best=max(best,1+dfs(j))
+            cache[i]=best
+            return cache[i]
+        return max(dfs(i) for i in range(n))
+
+# tabulation
+# O(n2) time, O(n) space
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        # state: dp[i] is length of LIS at end of i index in nums
+        dp=[1]*n
+        dp_best=1
+        for i in range(n):
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    dp[i]=max(dp[i],1+dp[j])
+            dp_best=max(dp_best,dp[i])
+        return dp_best
+
+# binary search (non-DP solution)
+# O(nlogn) time and O(n) space
+from bisect import bisect_left
+class SolutionV4:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        dp.append(nums[0])
+
+        LIS = 1
+        for i in range(1, len(nums)):
+            if dp[-1] < nums[i]:
+                dp.append(nums[i])
+                LIS += 1
+                continue
+
+            # lower bound binary search
+            idx = bisect_left(dp, nums[i])
+            dp[idx] = nums[i]
+
+        return LIS
