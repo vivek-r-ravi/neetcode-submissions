e5262f28e72e1e8bec7db7e04ddb188b50e8dff7
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-17 15:53:24 -0500

    Add: max-consecutive-ones - submission-3

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-3.py
@@ -0,0 +1,30 @@
+# solution 1: brute force O(n)
+class SolutionV1:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 0
+        best = 0
+        while i < n:
+            curr = 0
+            for j in range(i, n):
+                if nums[j] == 1:
+                    curr += 1
+                else:
+                    break
+            best = max(curr, best)
+            i = j + 1
+        return best
+
+
+# solution 2: efficient solution using prefix array O(n)
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        curr = 0
+        best = 0
+        for num in nums:
+            if num == 1:
+                curr += 1
+                best = max(best, curr)
+            else:
+                curr = 0
+        return best
