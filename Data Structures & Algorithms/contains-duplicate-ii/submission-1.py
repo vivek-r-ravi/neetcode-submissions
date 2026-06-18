d2dd381f3171bdfad590340614249466b4129104
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-18 18:52:07 -0500

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.py
@@ -0,0 +1,26 @@
+# brute force and sliding window
+# O(n2) time O(1) space
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        n=len(nums)
+        for L in range(n):
+            for R in range(L+1,min(n,L+k+1)):
+                if nums[L]==nums[R]:
+                    return True
+        return False
+
+# sliding window and set
+# O(n) time O(k) space
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        n=len(nums)
+        L=0
+        window=set()
+        for R in range(n):
+            if R-L>k:
+                window.remove(nums[L])
+                L+=1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False
