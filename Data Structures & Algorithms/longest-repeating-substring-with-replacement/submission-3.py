157ce86bd005db31ba3280809f19bd7f4acec9c5
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-19 15:49:31 -0500

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n = len(s)
+        length=0
+        for L in range(n):
+            rem=k
+            for R in range(L,n):
+                if s[L]!=s[R]:
+                    if rem>0:
+                        rem-=1
+                    else:
+                        break
+                length=max(length,R-L+1)
+            print((L,R,length,R-L+1))
+            while R==n-1 and L>=0:
+                if s[R]!=s[L]:
+                    if rem>0:
+                        rem-=1
+                    else:
+                        break
+                length=max(length,R-L+1)
+                L-=1
+        return length
+
+'''
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n = len(s)
+        length = 0
+        L = 0
+        for R in range(n):
+            if s[R] != s[L]:
+                L = R
+            length = max(length, R - L + 1)
+'''
