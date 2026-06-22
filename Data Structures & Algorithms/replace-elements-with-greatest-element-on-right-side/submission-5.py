12a207256ae8edd235a6db6473e58601d1a1bcda
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-22 15:11:53 -0500

    Add: replace-elements-with-greatest-element-on-right-side - submission-5

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        # solution 1: brute force array traversing O(n2)
+        """
+        for i in range(len(arr)):
+            if i==len(arr)-1:
+                arr[i]=-1
+            else:
+                arr[i]=arr[i+1]
+                for j in range(i+1,len(arr)):
+                    arr[i]=max(arr[i],arr[j])
+        return arr
+        """
+
+        # solution 2: array traversing with max in list function O(n2)
+        """
+        for i in range(len(arr)):
+            if i==len(arr)-1:
+                arr[i]=-1
+            else:
+                arr[i]=max(arr[i+1:])
+        return arr
+        """
+
+        # canonical solution: suffix max array O(n)
+        best = -1
+        for i in range(-1,-len(arr)-1,-1):
+            curr=arr[i]
+            arr[i]=best
+            best=max(curr,best)
+        return arr
