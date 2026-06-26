93544fa637c055b657e1497386ec45e8fc4cf04d
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-26 12:52:39 -0500

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,36 @@
+# brute force: find max left and right height for each index
+# O(n2) time O(1) space
+class SolutionV1:
+    def trap(self, height: List[int]) -> int:
+        n=len(height)
+        out=0
+        for i in range(1,n-1):
+            l=0
+            for j in range(i):
+                if height[j]>height[l]:
+                    l=j
+            r=len(height)-1
+            for k in range(i+1,n):
+                if height[k]>height[r]:
+                    r=k
+            vol=min(height[l],height[r])-height[i]
+            out+=vol if vol>0 else 0
+        return out
+
+
+# prefix and suffix max arrays
+# O(n) time O(n) space
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n=len(height)
+        out=0
+        prefix_max=[0]*n
+        suffix_max=[0]*n
+        for i in range(1,n):
+            prefix_max[i]=max(prefix_max[i-1],height[i-1])
+        for i in range(n-2,-1,-1):
+            suffix_max[i]=max(suffix_max[i+1],height[i+1])
+        for i in range(1,n-1):
+            vol=min(prefix_max[i],suffix_max[i])-height[i]
+            out+=vol if vol>0 else 0
+        return out         
