785cf94afd07a609c2318c7cc69c843d3e310fa7
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-30 14:43:06 -0500

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.py
@@ -0,0 +1,25 @@
+'''
+Problem:
+The next greater element of some element x in an array is the first greater element 
+that is to the right of x in the array.
+
+You are given two 0-indexed integer arrays nums1 and nums2.
+nums1 is a subset of nums2. Both contain unique elements.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j].
+Then, determine the next greater element of nums2[j] in nums2. 
+If there is no next greater element, then the answer for this query is -1.
+'''
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_map = {num: i for i, num in enumerate(nums1)}
+        ans = [-1] * len(nums1)
+        stack = []
+        for i in range(len(nums2)):
+            while len(stack) > 0 and stack[-1] < nums2[i]:
+                top = stack.pop()
+                if top in nums1_map:
+                    ans[nums1_map[top]] = nums2[i]
+            stack.append(nums2[i])
+        return ans
