ff5c01a44346ea67cac1c03a288016e6391d4eac
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-06-30 23:55:05 -0500

    Add: sliding-window-maximum - submission-7

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-7.py
@@ -0,0 +1,54 @@
+# check max every time in sliding window
+# O(n*k) time and O(k) space
+class SolutionV1:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        window = deque()
+        out = []
+        l = 0
+        for r in range(len(nums)):
+            if r - l + 1 < k:
+                window.append(nums[r])
+            elif r - l + 1 == k:
+                window.append(nums[r])
+                out.append(max(window))
+                window.popleft()
+                l += 1
+        return out
+
+
+# maintain max heap
+# O(nlogn) time and O(n) space
+class SolutionV2:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        max_heap = []
+        out = []
+        l = 0
+        for r in range(len(nums)):
+            if r - l + 1 < k:
+                heapq.heappush_max(max_heap, (nums[r], r))
+            elif r - l + 1 == k:
+                heapq.heappush_max(max_heap, (nums[r], r))
+                while max_heap[0][1] < l:
+                    heapq.heappop_max(max_heap)
+                out.append(max_heap[0][0])
+                l += 1
+        return out
+
+
+# monotonic deque
+# O(n) time and O(k) space
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        window = deque()  # decreasing deque with num and indicies
+        out = []
+        l = 0
+        for r in range(len(nums)):
+            while window and window[-1][0] < nums[r]:
+                window.pop()
+            window.append((nums[r], r))
+            while window[0][1] < l:
+                window.popleft()
+            if r - l + 1 == k:
+                out.append(window[0][0])
+                l += 1
+        return out
