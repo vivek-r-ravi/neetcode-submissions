5406000abd331952f73c5ac465d1913215309670
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-01 16:17:57 -0500

    Add: minimum-window-with-characters - submission-7

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-7.py
@@ -0,0 +1,34 @@
+# running match count on dynamic sliding window
+# O(n) time O(1) space
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        n = len(s)
+        m = len(t)
+
+        if n<m:
+            return ""
+
+        t_count = Counter(t)        
+        s_count = dict()
+
+        l_min, r_min = -1, n
+        matches = 0
+        l = 0
+        for r in range(n):
+            # expand right
+            if s[r] in t_count:
+                s_count[s[r]]=s_count.get(s[r],0)+1
+                if s_count[s[r]]>=t_count[s[r]] and s_count[s[r]]-1<t_count[s[r]]:
+                    matches+=1
+            
+            # check valid window and shrink left
+            while matches==len(t_count):
+                if r-l<r_min-l_min:
+                    l_min,r_min = l,r
+                if s[l] in t_count:
+                    s_count[s[l]]-=1
+                    if s_count[s[l]]<t_count[s[l]] and s_count[s[l]]+1>=t_count[s[l]]:
+                        matches-=1
+                l+=1
+        
+        return s[l_min:r_min+1] if r_min!=n else ""
