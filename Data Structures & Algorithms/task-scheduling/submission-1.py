5264d695c68b0f3a589e59416f974016d710387f
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-02 21:37:34 -0500

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,19 @@
+# heap + count hash map
+# O(n) time, O(1) space as heap size is at most 26
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # (-min_cycle, rem_count, task) in max heap
+        # min_cycle is number of cycles needed for task to be eligible for execution
+        heap = [(0, count, task) for task, count in Counter(tasks).items()]
+        heapq.heapify_max(heap)
+
+        # highest count tasks in lower layer (min_cycle) is highest priority
+        cycles = 0
+        while heap:
+            min_cycle, rem_count, task = heapq.heappop_max(heap)
+            cycles = max(cycles, -1 * min_cycle) + 1    # adds idle time if needed
+            rem_count -= 1
+            min_cycle -= n + 1                          # handle cooldown
+            if rem_count > 0:
+                heapq.heappush_max(heap, (min_cycle, rem_count, task))
+        return cycles
