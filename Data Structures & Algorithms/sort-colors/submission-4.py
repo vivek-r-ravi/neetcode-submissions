e2afea87596f73574ab5051ca2b1eb0300086cb0
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-05 18:55:20 -0500

    Add: sort-colors - submission-4

---
+++ Data Structures & Algorithms/sort-colors/submission-4.py
@@ -0,0 +1,37 @@
+# solution 1: use bucket sort
+# O(n) on time and O(3) on space
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = [0, 0, 0]  # for 0, 1, 2
+        for num in nums:
+            count[num] += 1
+        i = 0
+        for n in range(len(count)):
+            for j in range(count[n]):
+                nums[i] = n
+                i += 1
+
+
+# solution 2: Dutch National Flag solution using 3 pointers
+# O(n) on time and O(1) on space
+class SolutionV2:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums) - 1
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
