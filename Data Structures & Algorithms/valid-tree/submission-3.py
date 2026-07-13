1f15d6fca21f0c44d982068c919b52ac89361b96
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-13 16:03:55 -0500

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,64 @@
+# valid tree: no cycles, 1 connected component
+
+
+# DFS
+# O(V+E) time and space
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj_list = {}
+        for v1, v2 in edges:
+            if v1 not in adj_list:
+                adj_list[v1] = []
+            if v2 not in adj_list:
+                adj_list[v2] = []
+            adj_list[v1].append(v2)
+            adj_list[v2].append(v1)
+
+        # cycle detection using DFS with parent parameter
+        visited = set()
+        def dfs(v, par):
+            visited.add(v)
+            for nei in adj_list.get(v, []):
+                if nei == par:
+                    continue
+                if nei in visited:
+                    return True
+                if dfs(nei, v):
+                    return True
+            return False
+
+        return not dfs(0,-1) and len(visited) == n
+
+
+# union find
+# O(V+E) time O(V) space
+class SolutionV1:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        par = [i for i in range(n)]
+        rank = [0] * n
+        n_components = n
+
+        def find(v):
+            if par[v] != v:
+                par[v] = find(par[v])
+            return par[v]
+
+        def union(v1, v2):
+            p1, p2 = find(v1), find(v2)
+            if p1 == p2:
+                return False
+            if rank[p1] < rank[p2]:
+                par[p1] = p2
+            elif rank[p2] > rank[p1]:
+                par[p2] = p1
+            else:
+                par[p2] = p1
+                rank[p1] += 1
+            return True
+
+        for v1, v2 in edges:
+            if not union(v1, v2):
+                return False
+            else:
+                n_components -= 1
+        return n_components == 1
