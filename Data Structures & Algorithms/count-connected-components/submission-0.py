cd6d8351405df91e17f154769d917383420c5afc
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-13 14:08:20 -0500

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,36 @@
+"""
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+"""
+
+
+# union find
+# O(V+E) time O(V) space
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        par = [i for i in range(n)]
+        rank = [0] * n
+        n_components = n
+
+        def find(v):
+            if par[v] != v:
+                par[v] = find(par[v])
+            return par[v]
+
+        def union(v1, v2):
+            p1, p2 = find(v1), find(v2)
+            if p1 == p2:
+                return False
+            if rank[p1] < rank[p2]:
+                par[p1] = p2
+            elif rank[p2] > rank[p1]:
+                par[p2] = p1
+            else:
+                par[p2] = p1
+                rank[p1] += 1
+            return True
+
+        for v1, v2 in edges:
+            if union(v1, v2):
+                n_components -= 1
+        return n_components
