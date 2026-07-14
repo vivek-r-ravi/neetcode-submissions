28aff8b86bd39d99edabfb6d765eb10b2d37f42f
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-14 02:31:10 -0500

    Add: path-with-maximum-probability - submission-5

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-5.py
@@ -0,0 +1,23 @@
+# Dijkstra's with a max heap
+# O(ElogV) time O(V+E) space
+class Solution:
+    def maxProbability(
+        self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int
+    ) -> float:
+        adj = [[] for _ in range(n)]
+        for i in range(len(edges)):
+            adj[edges[i][0]].append((succProb[i], edges[i][1]))
+            adj[edges[i][1]].append((succProb[i], edges[i][0]))
+
+        max_heap = [(1, start_node)]
+        visited = set([start_node])
+        while max_heap:
+            p, node = heapq.heappop_max(max_heap)
+            if node == end_node:
+                return p
+            visited.add(node)
+            for prob, nei in adj[node]:
+                if nei not in visited:
+                    heapq.heappush_max(max_heap, (p * prob, nei))
+
+        return 0
