bd851e4b8c61b94c0102ca3832f797dbe44e2a50
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-14 01:44:07 -0500

    Add: dijkstra - submission-2

---
+++ Data Structures & Algorithms/dijkstra/submission-2.py
@@ -0,0 +1,27 @@
+# O(ElogV) time and O(V+E) space
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        # array of edges to adjList
+        adjList = {i: [] for i in range(n)}
+        for st, end, w in edges:
+            adjList[st].append((end, w))
+
+        # compute shortest paths
+        heap = []
+        heapq.heappush(heap, (0, src))
+        dist = {}
+        while heap:
+            w_curr, curr = heapq.heappop(heap)
+            if curr in dist:
+                continue
+            dist[curr] = w_curr
+            for neighbor, w in adjList[curr]:
+                if neighbor not in dist:
+                    heapq.heappush(heap, (w + w_curr, neighbor))
+
+        # fill in missing vertices
+        for i in range(n):
+            if i not in dist:
+                dist[i] = -1
+
+        return dist
