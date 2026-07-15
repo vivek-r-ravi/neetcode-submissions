7e0f0a46cf34dcdfad908209ceb4eb94a7c328cd
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-15 12:44:24 -0500

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.py
@@ -0,0 +1,15 @@
+# canonical solution: Bellman Ford after k+1 passes
+# O(kE) time, O(V) space
+class Solution:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
+        cost = [float("inf")] * n
+        cost[src] = 0
+        for _ in range(k + 1):      # k stop mean k+1 vertices
+            cost_copy = cost[:]     # using copy to only traverse vertices k+1 edges away
+            for u, v, w in flights:
+                if cost[u] != float("inf") and cost[u] + w < cost_copy[v]:
+                    cost_copy[v] = cost[u] + w
+            cost = cost_copy
+        return cost[dst] if cost[dst] != float("inf") else -1
