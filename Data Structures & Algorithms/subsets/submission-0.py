bd7da871be57a82556d334715e09fc1dc12db5f0
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-16 00:25:11 -0500

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subset, out = [], []
+
+        def helper(i):
+            if i == len(nums):
+                out.append(subset.copy())
+                return
+
+            # decision to include nums[i]
+            subset.append(nums[i])
+            helper(i + 1)
+            subset.pop()
+
+            # decision to exclude nums[i]
+            helper(i + 1)
+
+        helper(0)
+        return out
