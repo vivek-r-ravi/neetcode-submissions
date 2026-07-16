949d294da1caa9c51cd1969f987a7b76e29668d0
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-16 13:40:48 -0500

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,21 @@
+# backtracking with running total
+# exponential time, O(t/m) space where t is target and m in min(nums)
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        combo, out = [], []
+
+        def dfs(i, total):
+            if total == target:
+                out.append(combo.copy())
+                return
+
+            for j in range(i, len(nums)):
+                if total + nums[j] > target:
+                    break
+                combo.append(nums[j])
+                dfs(j, total + nums[j])
+                combo.pop()
+
+        dfs(0, 0)
+        return out
