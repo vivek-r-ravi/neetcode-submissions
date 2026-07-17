61c54bc78c5084083c6c80d874a77c9fe5c96617
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-17 00:42:22 -0500

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,42 @@
+# backtracking with visited set
+# O(n*n!) time O(n) space
+class SolutionV1:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        out, path, visited = [], [], set()
+
+        def dfs():
+            if len(path) == len(nums):
+                out.append(path.copy())
+                return
+
+            for j in range(len(nums)):
+                if j in visited:
+                    continue
+                path.append(nums[j])
+                visited.add(j)
+                dfs()
+                visited.remove(j)
+                path.pop()
+
+        dfs()
+        return out
+
+
+# backtracking with permutations in place
+# O(n*n!) time O(n) space
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        out = []
+
+        def dfs(i):
+            if i == len(nums):
+                out.append(nums.copy())
+                return
+
+            for j in range(i, len(nums)):
+                nums[j], nums[i] = nums[i], nums[j]
+                dfs(i + 1)
+                nums[j], nums[i] = nums[i], nums[j]
+
+        dfs(0)
+        return out
