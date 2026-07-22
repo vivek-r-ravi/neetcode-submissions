d372cf75184fad3a342fea4377276a0c1dd1fe6a
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-22 16:52:16 -0500

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        out, path = [], []
+        n = len(s)
+
+        def dfs(i):
+            if i >= n:
+                out.append(path.copy())
+                return
+
+            for j in range(i, n):
+                if not self.is_palindrome(s[i : j + 1]):
+                    continue
+                path.append(s[i : j + 1])
+                dfs(j + 1)
+                path.pop()
+
+        dfs(0)
+        return out
+
+    def is_palindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
