53b06a07763936c0a4f8c69e953001b933edcd00
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-07-22 20:39:53 -0500

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        out, path, pos_diag, neg_diag, cols = [], [], set(), set(), set()
+
+        def dfs(r):
+            if r == n:
+                out.append(path.copy())
+                return
+
+            for c in range(n):
+                if c in cols or r + c in pos_diag or r - c in neg_diag:
+                    continue
+                row = "." * c + "Q" + "." * (n - c - 1)
+                path.append(row)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                cols.add(c)
+                dfs(r + 1)
+                cols.remove(c)
+                neg_diag.remove(r - c)
+                pos_diag.remove(r + c)
+                path.pop()
+
+        dfs(0)
+        return out
