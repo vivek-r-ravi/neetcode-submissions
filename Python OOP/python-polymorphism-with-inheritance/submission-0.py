e3d380c8c2fc1b97b0eac054e18f38a052da5f5f
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-08-04 17:42:17 -0500

    Add: python-polymorphism-with-inheritance - submission-0

---
+++ Python OOP/python-polymorphism-with-inheritance/submission-0.py
@@ -0,0 +1,31 @@
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+
+    def make_sound(self) -> None:
+        print("Animal is making a sound")
+
+
+class Dog(Animal):
+    def make_sound(self) -> None:
+        print(f"{self.name} says: Woof!")
+
+
+class Cat(Animal):
+    def make_sound(self) -> None:
+        print(f"{self.name} says: Meow!")
+
+
+def animal_sound(animal: Animal) -> None:
+    animal.make_sound()
+
+
+# Do not change the code below
+animal = Animal("Rabbit")
+animal.make_sound()
+
+animal = Dog("Buddy")
+animal.make_sound()
+
+animal = Cat("Whiskers")
+animal.make_sound()
