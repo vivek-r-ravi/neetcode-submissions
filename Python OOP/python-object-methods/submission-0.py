23a4416c38eaca93bfc4dd009ab6d432bad61bd0
Vivek Ravi <65517839+vivek-r-ravi@users.noreply.github.com>
2026-08-04 14:49:30 -0500

    Add: python-object-methods - submission-0

---
+++ Python OOP/python-object-methods/submission-0.py
@@ -0,0 +1,17 @@
+class Pet:
+    def __init__(self, name: str):
+        self.name = name
+        self.hunger = 5
+
+    def feed(self):
+        self.hunger -= 1
+        print(f"{self.name} has been fed.")
+        print(f"{self.name}'s hunger level: {self.hunger}")
+
+
+# Create a pet
+my_pet = Pet("Fluffy")
+
+# TODO: Feed the pet three times
+for _ in range(3):
+    my_pet.feed()
